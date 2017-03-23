a0bb3563dc563988d575eed6af314caf0ffa0de3
zackn9ne@gmail.com <zackn9ne@gmail.com>
2017-03-23 19:33:33 +0100

    workin' under xampp

--- login/dbconf.php
+++ login/dbconf.php
@@ -1,8 +1,8 @@
 <?php
 //DATABASE CONNECTION VARIABLES
 $host = "localhost"; // Host name
-$username = "user"; // Mysql username
-$password = "password"; // Mysql password
+$username = "root"; // Mysql username
+$password = ""; // Mysql password
 $db_name = "login"; // Database name
 
 //DO NOT CHANGE BELOW THIS LINE UNLESS YOU CHANGE THE NAMES OF THE MEMBERS AND LOGINATTEMPTS TABLES
