3b597d6852ce9c86a6259b5a53dec7a2b4909571
zackn9ne@gmail.com <zackn9ne@gmail.com>
2017-03-23 20:01:14 +0100

    set env var with export PASS='secrets

--- login/dbconf.php
+++ login/dbconf.php
@@ -1,12 +1,18 @@
 <?php
+$pass = getenv("PASS");
+
+
 //DATABASE CONNECTION VARIABLES
 $host = "localhost"; // Host name
 $username = "root"; // Mysql username
-$password = ""; // Mysql password
+$password = $pass; // Mysql password
 $db_name = "login"; // Database name
 
-//DO NOT CHANGE BELOW THIS LINE UNLESS YOU CHANGE THE NAMES OF THE MEMBERS AND LOGINATTEMPTS TABLES
+//DO NOT CHANGE BELOW THIS LINE UNLESS YOU CHANGE THE NAMES OF 
+THE MEMBERS AND LOGINATTEMPTS TABLES
 
-$tbl_prefix = ""; //***PLANNED FEATURE, LEAVE VALUE BLANK FOR NOW*** Prefix for all database tables
+$tbl_prefix = ""; //***PLANNED FEATURE, LEAVE VALUE BLANK FOR 
+NOW*** Prefix for all database tables
 $tbl_members = $tbl_prefix."members";
 $tbl_attempts = $tbl_prefix."loginAttempts";
+
